4db3701558a4949bda3df920ac2059a579d6dffc
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2020-12-23 14:04:07 +0100

    footer / top-nav JS
    new footer in 3 columns;
    top-nav center and only seach button

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -5,11 +5,29 @@ class Footer extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <footer class="footer bg-dark">
-          <div class="container">
-           <span class="text-muted">
-              By Skybar  
-            </span>
+        <footer class="pt-4 my-md-5 pt-md-5">
+          <div class="row justify-content-md-center">
+            <div class="col-md-auto">
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Blog</a></li>
+                <li><a class="text-muted" href="#">FAQ</a></li>
+                <li><a class="text-muted" href="#">Donate</a></li>
+              </ul>
+            </div>
+            <div class="col-md-auto">
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Top seller</a></li>
+                <li><a class="text-muted" href="#">Top Realm</a></li>
+                <li><a class="text-muted" href="#">Top Profession</a></li>
+              </ul>
+            </div>
+            <div class="col-md-auto">
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Work</a></li>
+                <li><a class="text-muted" href="#">Terms</a></li>
+                <li><a class="text-muted" href="#">Contac</a></li>
+              </ul>
+            </div>
           </div>
         </footer>
         `;
--- frontend/components/top-nav.js
+++ frontend/components/top-nav.js
@@ -5,26 +5,21 @@ class TopNav extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
+
         <nav class="navbar navbar-expand-sm fixed-top bg-dark navbar-dark">
-        <ul class="navbar-nav"> <!-- don't work justify-content-center -->
-          <li class="nav-item active">
-            <a class="nav-link" href="#about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#work">Work</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#contact">Contact</a>
-          </li>
-        </ul> 
-        <!-- search button  -->
-        <div class="container-fluid justify-content-end">
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search database" aria-label="Search">
-            <button class="btn btn-outline-light" type="submit">Go!</button>
-          </form>
-        </div>
-      </nav>
+          <!-- search button  -->
+          <div class="container-fluid justify-content-center">
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search database" aria-label="Search">
+              <button class="btn btn-outline-light" type="submit">Go!</button>
+            </form>
+            <ul class="navbar-nav">
+              <il class="nav-item">
+                <a class="nav-link" href="#">Token price</a>
+              </il>
+            </ul>
+          </div>
+        </nav>
         `;
       }
 }
