68ef07ad5947798c1fea3042888d7c15de1470ec
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-01-10 20:03:14 +0100

    Update AuctionHouse.page.js
    Añado div del sidebar

--- frontend/src/pages/AuctionHouse.page.js
+++ frontend/src/pages/AuctionHouse.page.js
@@ -5,6 +5,7 @@ import { Navbar, Sidebar, Table } from '../components';
 export default function AuctionHouse() {
   return (
     <div class="d-flex" id="wrapper">
+      <div class="bg-dark border-right" id="sidebar-wrapper"></div>
       <Sidebar />
       <div id="page-content-wrapper">
         <Navbar />
