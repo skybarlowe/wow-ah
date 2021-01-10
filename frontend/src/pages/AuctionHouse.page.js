a3fb4a5590bc06e836c7f99bf7d1dd0eebc936f0
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-01-10 21:36:39 +0100

    Update div AuctionHouse.page.js
    He ordenado los <div> de todo el contenido de la página

--- frontend/src/pages/AuctionHouse.page.js
+++ frontend/src/pages/AuctionHouse.page.js
@@ -5,10 +5,17 @@ import { Navbar, Sidebar, Table } from '../components';
 export default function AuctionHouse() {
   return (
     <div class="d-flex" id="wrapper">
-      <div class="bg-dark border-right" id="sidebar-wrapper"></div>
-      <Sidebar />
+      <div class="bg-dark border-right" id="sidebar-wrapper">
+        <Sidebar />
+      </div>
       <div id="page-content-wrapper">
-        <Navbar />
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
+          <Button />
+          <BUttonCollapseMenu />
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <Navbar />
+          </div>  
+        </div>  
         <div class="container-fluid">
           <Table />
         </div>
