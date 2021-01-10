a2377d82131f14a6ef8f7857e81a4cfb0645d4b7
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-01-10 20:17:24 +0100

    Update Table.js
    Añado tabla de items de 3 columnas (precio, nombre y disponibilidad)

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,3 +1,120 @@
 export default function Table() {
-  return null;
+  return (
+    <table class="table table-hover" id="tableAH">
+      <thead>
+        <tr>
+          <th scope="col">Price</th>
+          <th scope="col">Name</th>
+          <th scope="col">Available</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <th scope="row">200</th>
+            <td>Sword Sword Sword Sword Sword </td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <th scope="row">10000</th>
+            <td>Pet</td>
+            <td>24</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Enchant</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <th scope="row">200</th>
+            <td>Sword Sword Sword Sword Sword </td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <th scope="row">10000</th>
+            <td>Pet</td>
+            <td>24</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Enchant</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <th scope="row">200</th>
+            <td>Sword Sword Sword Sword Sword </td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <th scope="row">10000</th>
+            <td>Pet</td>
+            <td>24</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Enchant</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <th scope="row">200</th>
+            <td>Sword Sword Sword Sword Sword </td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <th scope="row">10000</th>
+            <td>Pet</td>
+            <td>24</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Enchant</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <th scope="row">200</th>
+            <td>Sword Sword Sword Sword Sword </td>
+            <td>12</td>
+        </tr>
+        <tr>
+          <th scope="row">10000</th>
+          <td>Pet</td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Enchant</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <th scope="row">200</th>
+          <td>Sword Sword Sword Sword Sword </td>
+          <td>12</td>
+        </tr>
+        <tr>
+          <th scope="row">10000</th>
+          <td>Pet</td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Enchant</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <th scope="row">200</th>
+          <td>Sword Sword Sword Sword Sword </td>
+          <td>12</td>
+        </tr>
+        <tr>
+          <th scope="row">10000</th>
+          <td>Pet</td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Enchant</td>
+          <td>10</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
