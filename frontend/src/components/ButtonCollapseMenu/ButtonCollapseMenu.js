5667abc81475836db8cd9c4baf87f793bdc591eb
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-01-10 21:08:05 +0100

    Create ButtonCollapseMenu
    Este componente aparece en dispositivos móviles ocultando el menú

---
+++ frontend/src/components/ButtonCollapseMenu/ButtonCollapseMenu.js
@@ -0,0 +1,15 @@
+export default function ButtonCollapseMenu(){
+    return (
+        <button 
+            class="navbar-toggler" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+
+            <span class="navbar-toggler-icon"></span>          
+        </button>
+    );
+}
