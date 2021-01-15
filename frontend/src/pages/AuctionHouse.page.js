1d6b14fdf6f76ca8bd1f106f30a8fad1624df09f
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-01-15 15:34:34 +0100

    Update AuctionHouse.page.js
    I create items list, and with React I call them using props with Table.js

--- frontend/src/pages/AuctionHouse.page.js
+++ frontend/src/pages/AuctionHouse.page.js
@@ -1,19 +1,52 @@
 import React from 'react';
 
-import { Navbar, Sidebar, Table } from '../components';
+import { Navbar, Sidebar, Table} from '../components';
+
+  const colPrice = "Price";
+  const colName = "Item name";
+  const colAvailable = "Available";
+
+  const items = [{
+    id: 107264,
+    price: 500,
+    name: "sword",
+    available: 12,
+  },
+  {
+    id: 107264,
+    price: 5000,
+    name: "Shell",
+    available: 24,
+  }
+];
 
 export default function AuctionHouse() {
   return (
-    <div class="d-flex" id="wrapper">
+    <div className="d-flex" id="wrapper">
       {/* Sidebar */}
-      <div class="bg-dark border-right" id="sidebar-wrapper">
+      <div className="bg-dark border-right" id="sidebar-wrapper">
         <Sidebar />
       </div>
       {/* Page Content */}
       <div id="page-content-wrapper">
         <Navbar />
-      <div class="container-fluid">
-        <Table />
+      <div className="container-fluid">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">{colPrice}</th>
+              <th scope="col">{colName}</th>
+              <th scope="col">{colAvailable}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => { 
+              return (
+                <Table {...item} />
+              );
+            })}
+          </tbody>
+        </table>
       </div>
       </div>
     </div>
