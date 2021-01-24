90e6bd869f5b926eee2ad2d00bd8841c511938b7
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-01-24 11:22:18 +0100

    Update Navbar.js
    Ajusto para un Serach

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,40 +1,23 @@
-export default function Navbar() {
-  
-
-  function onClick() {
-    document.getElementById("wrapper").classList.toggle("toggled");
-  }
-
+export default function Navbar({ searchTerm, handleChange }) {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
-        <button onClick={onClick} class="btn btn-light" id="menu-toggle">
-          <i class="fa fa-bars"></i>
-        </button>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <input
-              class="form-control form-control-dark mr-sm-2"
-              type="text"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+        <div className="float-sm-left navbar-collapse">
+          {/* <ul className="navbar-nav ml-auto"> */}
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+          {/* <button
+              className="btn btn-outline-light my-2 my-sm-0"
+              type="submit"
+            >
               Search
-            </button>
-          </ul>
+            </button> */}
+          {/* </ul> */}
         </div>
       </nav>
     </div>
