a8f1e0762ad5dca344bf2cef93645edb3fcd7296
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-01-24 11:22:06 +0100

    Update Table.js
    Actualizo para mostrar 1 columna y los resultados de realmList

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,22 +1,18 @@
-export default function Table({items}) {
+export default function Table({ items, searchTerm, handleChange }) {
   return (
     <table className="table table-hover">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Available</th>
-      </tr>
-    </thead>
-    <tbody>
-      {items.map((item) => (
-        <tr key={item.id}>
-          <td><a href={"https://es.wowhead.com/item="+item.item.id} target="_blank"></a></td>
-          <td>{item.buyout}</td>
-          <td>{item.quantity}</td>
+      <thead>
+        <tr>
+          <th>Realm</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
