8d061267fe4929ac6bf3cb85b6038b6fd013994b
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-02-01 09:55:34 +0100

    Update Navbar
    Intento utilizar ReactDOM.render para añadir el componente pero sin éxito

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
+import RealmSuggest from "../RealmSuggest/RealmSuggest";
+import ReactDOM from "react-dom";
+
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar-nav navbar-expand-sm navbar-dark bg-dark">
         {/* <div className="container-fluid"> */}
         <ul className="navbar-nav ml-auto mx-5">
-          {/* <li class="nav-item">{props}</li> */}
+          <li class="nav-item" id="auto-suggest"></li>
           <li class="nav-item">
             <button className="btn btn-light m-1">Contact us</button>
           </li>
@@ -17,3 +20,5 @@ export default function Navbar() {
     </div>
   );
 }
+
+// ReactDOM.render(<RealmSuggest />, document.getElementById("auto-suggest"));
--- frontend/src/pages/RealmList.page.js
+++ frontend/src/pages/RealmList.page.js
@@ -13,6 +13,7 @@ export default function RealmList() {
         </div>
       </div> */}
       <div>
+        {/* <Navbar /> */}
         <RealmSuggest />
       </div>
       {/* <Navbar props={RealmSuggest} /> */}
